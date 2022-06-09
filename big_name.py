f55e3a8c32caba66f6f1b64f7ed8a2edcf45b3bf
SaNta1005 <107103829+SaNta1005@users.noreply.github.com>
2022-06-09 17:36:33 +0300

    Add files via upload

---
+++ big_name.py
@@ -0,0 +1,9 @@
+def printMyBigName():
+    print(' PPPPP   OOOO   M      M        A      H    H')
+    print(' P    P O    O M  M   M  M     A A     H    H')
+    print(' P    P O    O M   M M   M    A   A    H    H')
+    print(' PPPPP  O    O M    M    M   AAAAAAA   HHHHHH')
+    print(' P      O    O M         M  A       A  H    H')
+    print(' P       OOOO  M         M A         A H    H')
+for i in range(5):
+    printMyBigName()
